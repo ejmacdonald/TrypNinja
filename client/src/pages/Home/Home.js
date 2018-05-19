70a688bf848968f523465d461e391b93fc9ad130
TheSamLloyd <samlloydj@gmail.com>
2018-05-19 13:09:01 -0400

    added home page changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,53 +1,48 @@
 import React, { Component } from 'react';
 import TitleBar from "../../components/TitleBar";
 import UserTile from "../../components/UserTile";
-import UserList from "../../image.json";
+//import UserList from "../../image.json";
+import UserStoryTile from "../../components/UserStoryTile"
+import StoryList from "../../components/StoryList"
 import axios from 'axios'
-
-console.log("UserList: " + JSON.stringify(UserList));
+const thispage="home"
 
 class App extends Component {
   constructor(){
     super()
-    this.state = { userList: null, key:Math.random()}
+    this.state = { storyList: [] }
   }
-  getUsers = ()=>{
-    axios.get("/api/user/all")
-      .then(users => {
-        if (users.data !== null) {
-          this.setState({ userList: users.data })
-        }
-        else {
-          this.setState({ userList: null })
-        }
+  getStories = ()=>{
+    axios.get("/api/event/all")
+      .then(events => {
+        console.log(events.data)
+        console.log(events.data[0].Moments[0].moment)
+        this.setState({storyList:events.data})
       })
   }
   componentWillMount(){
-    this.getUsers()
-    if (this.state.userList==null){
-      this.setState({userList:UserList})
-    }
+    this.getStories()
   }
   render() {
     return (
       <div className="wrapper">
         <TitleBar
           showBtn={true}
-          addUser={this.getUsers}
+          addUser={null}
         />
 
-        <div className="card-deck">
-          {this.state.userList.map((user) => (
-            <UserTile
-              key={user.id}
-              id={user.id}
-              name={user.userName}
-              src={user.profileImg+"?sz=500"}
-              imageClick={this.imageClick}
-            />
-          ))}
-        </div>
-
+        {this.state.storyList.map((story) => (
+          <UserStoryTile
+            key={story.id}
+            id={story.id}
+            title={story.title}
+            src={story.Moments[0] ? story.Moments[0].moment : "https://media.istockphoto.com/photos/black-zipper-and-leather-texture-close-up-background-picture-id647171256?s=2048x2048"}
+            imageClick={this.imageClick}
+            userName={story.User.userName}
+            origin={thispage}
+          />
+        ))}
+        <StoryList />
       </div>
     );
   }
