7703adf9b00bf68335a914294eabda28f02d5027
scull1916 <kpscully@gmail.com>
2018-05-20 11:28:25 -0400

    materialUI carousel built out

--- client/src/pages/Story/Story.js
+++ client/src/pages/Story/Story.js
@@ -10,76 +10,115 @@ import KeyboardArrowLeft from '@material-ui/core/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/core/KeyboardArrowRight';
 import Story from "../../story1.json";
 import TitleBar from "../../components/TitleBar";
+import { Typography } from '@material-ui/core';
 
 const momentSwipe = [
   {
     label: 'Picture 1',
-    imgPath: Story.[0]
+    imgPath: Story
   }
-]
+];
 
-// let storyArray = Story;
+const styles = theme => ({
+  root: 
+  {
+    maxWidth: 400,
+    flexGrow: 1,
+  },
+  header:
+  {
+    display: 'flex',
+    alignItems: 'center',
+    height: 50,
+    paddingLeft: theme.spacing.unit * 4,
+    marginBottom: 20,
+    backgroundColor: theme.palette.background.default,    
+  },
+  img: 
+  {
+    height: 255,
+    maxWidth: 400,
+    overflow: 'hidden',
+    width: '100%',
+  },
+});
 
-export default class extends React.Component {
-    render() {
-      return (
+class TextMobileStepper extends React.Component
+{
+  state = 
+  {
+    activeStep: 0,
+  };
 
-        <div className="wrapper">
+  handleNext = () =>
+  {
+    this.setState(prevState =>
+    ({
+      activeStep:prevState.activeStep + 1,
+    }));
+  };
 
-        {/* <TitleBar/> */}
-        <CarouselProvider
-          naturalSlideWidth={100}
-          naturalSlideHeight={150}
-          totalSlides={3}
-          touchEnabled="true"
-          isPlaying="false"
-          interval="10000000"
-        >        
-          <Slider>
-            <Slide index={0}>First
-                <div class="image-div">
-                <img class="story-img" alt="img" src={require("../../images/eric/" + storyArray[0].image)} />
-                </div>
-            </Slide>
-            <Slide index={1}>I am the second Slide.
-                <div class="image-div">
-                <img class="story-img" alt="img" src={require("../../images/eric/" + storyArray[1].image)} />
-                </div>
-            </Slide>
-            <Slide index={2}>I am the third Slide.
-                <div class="image-div">
-                <img class="story-img" alt="img" src={require("../../images/eric/" + storyArray[2].image)} /> 
-                </div>           
-            </Slide>
-          </Slider>
-          <ButtonBack>Back</ButtonBack>
-          <ButtonNext>Next</ButtonNext>
-        </CarouselProvider>
-        </div>
-      );
-    }
+  handleBack = () =>
+  {
+    this.setState(prevState =>
+    ({
+      activeStep: prevState.activeStep - 1,
+    }));
+  };
+
+  render()
+  {
+    const { classes, theme } = this.props;
+    const { activeStep } = this.state;
+
+    const maxSteps = momentSwipe.length;
+
+    return
+    (
+      <div className={classes.root}>
+        <Paper square elevation={0} className={classes.header}>
+          <Typography>{momentSwipe[activeStep].label}
+          </Typography>
+        </Paper>
+        <img
+          className={classes.img}
+          src={momentSwipe[activeStep].imgPath}
+          alt={momentSwipe[activeStep].label}
+        />
+        <MobileStepper
+          variant="text"
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+          className={classes.mobileStepper}
+          nextButton=
+          {
+            <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps -1}>
+             Next
+             {
+               theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />
+             }
+               </Button>
+             }
+          backButton=
+          {
+            <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
+              {
+                theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />
+              }
+              Back
+            </Button>
+          }
+        />
+      </div>
+    );
   }
+}
 
+TextMobileStepper.propTypes = 
+{
+  classes: PropTypes.object.isRequried,
+  theme: PropTypes.object.isRequried,
+};
 
-// class DemoCarousel extends Component {
-//     render() {
-//         return (
-//             <Carousel showArrows={true}>
-//                 <div>
-//                     <img alt="img" src={require("../../images/moab/" + storyArray[0].image)} />
-//                     <p className="legend">Photo 1</p>
-//                 </div>
-//                 <div>
-//                     <img alt="img" src={require("../../images/moab/" + storyArray[1].image)} />
-//                     <p className="legend">Photo 2</p>
-//                 </div>
-//                 <div>
-//                     <img alt="img" src={require("../../images/moab/" + storyArray[2].image)} />
-//                     <p className="legend">Photo 3</p>
-//                 </div>
-//             </Carousel>
-//         );
-//     }
-// };
- 
-// ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
+export default withStyles(styles, { withTheme: true })(TextMobileStepper);
