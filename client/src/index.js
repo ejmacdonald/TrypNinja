e548230c2bd0fae672eb64e414800c00ab863052
scull1916 <kpscully@gmail.com>
2018-05-20 19:23:03 -0400

    working to get @material-ui up & running

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import SwipeableViews from 'react-swipeable-views';
 import style from './style/style.css'
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- client/src/pages/Story/Story.js
+++ client/src/pages/Story/Story.js
@@ -1,41 +1,53 @@
 import React from 'react';
-//Material UI code
 import PropTypes from 'prop-types';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/core/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/core/KeyboardArrowRight';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
 import Story from "../../story1.json";
 import TitleBar from "../../components/TitleBar";
-import { Typography } from '@material-ui/core';
-import SwipeableViews from 'react-swipeable-views';
 
-const momentSwipe = [
+const swipePix = [
   {
     label: 'Picture 1',
-    imgPath: Story
-  }
+    imgPath: '/images/bad-logo.png',
+  },
+  {
+    label: 'Picture 2',
+    imgPath: '/images/dog1.jpg',
+  },
+  {
+    label: 'Picture 3',
+    imgPath: '/images/dog2.jpg',
+  },
+  {
+    label: 'Picture 4',
+    imgPath: '/images/dog3.jpg',
+  },
+  {
+    label: 'Picture 5',
+    imgPath: '/images/dog5.jpg',
+  },
 ];
 
 const styles = theme => ({
-  root: 
-  {
+  root: {
     maxWidth: 400,
     flexGrow: 1,
   },
-  header:
-  {
+  header: {
     display: 'flex',
     alignItems: 'center',
     height: 50,
     paddingLeft: theme.spacing.unit * 4,
     marginBottom: 20,
-    backgroundColor: theme.palette.background.default,    
+    backgroundColor: theme.palette.background.default,
   },
-  img: 
-  {
+  img: {
     height: 255,
     maxWidth: 400,
     overflow: 'hidden',
@@ -43,69 +55,63 @@ const styles = theme => ({
   },
 });
 
-class TextMobileStepper extends React.Component
-{
-  state = 
-  {
+class SwipeableTextMobileStepper extends React.Component {
+  state = {
     activeStep: 0,
   };
 
-  handleNext = () =>
-  {
-    this.setState(prevState =>
-    ({
-      activeStep:prevState.activeStep + 1,
+  handleNext = () => {
+    this.setState(prevState => ({
+      activeStep: prevState.activeStep + 1,
     }));
   };
 
-  handleBack = () =>
-  {
-    this.setState(prevState =>
-    ({
+  handleBack = () => {
+    this.setState(prevState => ({
       activeStep: prevState.activeStep - 1,
     }));
   };
 
-  render()
-  {
+  handleStepChange = activeStep => {
+    this.setState({ activeStep });
+  };
+
+  render() {
     const { classes, theme } = this.props;
     const { activeStep } = this.state;
 
-    const maxSteps = momentSwipe.length;
+    const maxSteps = swipePix.length;
 
-    return
-    (
+    return (
       <div className={classes.root}>
         <Paper square elevation={0} className={classes.header}>
-          <Typography>{momentSwipe[activeStep].label}
-          </Typography>
+          <Typography>{swipePix[activeStep].label}</Typography>
         </Paper>
-        <img
-          className={classes.img}
-          src={momentSwipe[activeStep].imgPath}
-          alt={momentSwipe[activeStep].label}
-        />
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={this.state.activeStep}
+          onChangeIndex={this.handleStepChange}
+          enableMouseEvents
+        >
+          {swipePix.map(step => (
+            <img key={step.label} className={classes.img} src={step.imgPath} alt={step.label} />
+          ))}
+        </SwipeableViews>
         <MobileStepper
           variant="text"
           steps={maxSteps}
           position="static"
           activeStep={activeStep}
           className={classes.mobileStepper}
-          nextButton=
-          {
-            <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps -1}>
-             Next
-             {
-               theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />
-             }
-               </Button>
-             }
-          backButton=
-          {
+          nextButton={
+            <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps - 1}>
+              Next
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
             <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
-              {
-                theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />
-              }
+              {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
               Back
             </Button>
           }
@@ -115,10 +121,9 @@ class TextMobileStepper extends React.Component
   }
 }
 
-TextMobileStepper.propTypes = 
-{
-  classes: PropTypes.object.isRequried,
-  theme: PropTypes.object.isRequried,
+SwipeableTextMobileStepper.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles, { withTheme: true })(TextMobileStepper);
+export default withStyles(styles, { withTheme: true })(SwipeableTextMobileStepper);
