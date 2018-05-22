d2076426ed051649da5fb431fe053489cdf7d4c8
TheSamLloyd <samlloydj@gmail.com>
2018-05-21 20:56:29 -0400

    edits to user story

--- client/src/components/UserStoryTile/UserStoryTile.js
+++ client/src/components/UserStoryTile/UserStoryTile.js
@@ -1,30 +1,29 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import AddButton from "../AddButton";
 
 
 const UserStoryTile = (props) => (
 
 
-<div>
+  <div>
     <nav className="navbar navbar-top navbar-light bg-light">
-    {(props.origin === "userPage") ? 
-      <Link
-        to="/story"
-        className={window.location.pathname==="/story" ? "nav-link active" : "nav-link"}
-      >
-        <img className="thumb"  alt={props.id} src={require("../../images/" + props.src)}/>
-                {props.title} 
-        <a className="navbar-brand"></a>
-      </Link>
-      : <Link
-      to="/choosecontenttype"
-      className={window.location.pathname==="/choosecontenttype" ? "nav-link active" : "nav-link"}
-    >
-      <img className="thumb"  alt={props.id} src={props.src}/>
-              {props.title} by {props.userName}
-      <a className="navbar-brand"></a>
-    </Link>}
+      {(props.origin === "userPage") ?
+        <Link
+          to="/story"
+          className={window.location.pathname === "/story" ? "nav-link active" : "nav-link"}
+        >
+          <img className="thumb" alt={props.id} src={require("../../images/" + props.src)} />
+          {props.title}
+          <a className="navbar-brand"></a>
+        </Link>
+        : <div className={window.location.pathname === "/choosecontenttype" ? "nav-link active" : "nav-link"}>
+          <img className="thumb" alt={props.id} src={props.src} />
+          <Link to="/choosecontenttype" className={window.location.pathname === "/choosecontenttype" ? "nav-link active" : "nav-link"}>
+            {props.title}
+          </Link>
+          <Link to="/userPage"> {props.userName} </Link>
+        </div>}
 
     </nav>
   </div>
